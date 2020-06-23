1843af2d465227ddb4fd8971bf6e0fa4a981b53e
mathru <support@mathru.net>
2020-06-23 13:11:40 +0900

    FirebaseVirifier（Android）の追加

---
+++ lib/firebaseverifier.dart
@@ -0,0 +1,62 @@
+part of masamune.purchase.firebase;
+
+/// [PurchaseCore] is used as a callback for [onVerify] of [PurchaseCore].
+///
+/// The signature is verified and the receipt is verified by firebase.
+class FirebaseVerifier {
+
+  /// [PurchaseCore] is used as a callback for [onVerify] of [PurchaseCore].
+  ///
+  /// The signature is verified and the receipt is verified by firebase.
+  ///
+  /// [purchase]: PurchaseDetails.
+  /// [product]: The purchased product.
+  /// [core]: Purchase Core instance.
+  static Future<bool> verify( PurchaseDetails purchase, PurchaseProduct product,
+      PurchaseCore core ) async {
+          if (Config.isAndroid) {
+      if (core.androidVerifierOptions == null ||
+          isEmpty(core.androidRefreshToken) ||
+          isEmpty(core.androidVerifierOptions.clientId) ||
+          isEmpty(core.androidVerifierOptions.clientSecret) ||
+          isEmpty(core.androidVerifierOptions.publicKey)) return false;
+      if (!await verifyString(
+          purchase.verificationData.localVerificationData,
+          purchase.billingClientPurchase.signature,
+          core.androidVerifierOptions.publicKey)) return false;
+          FunctionsTask task = await FunctionsTask.call(
+            core.androidVerifierOptions.verificationServer,
+            postData: {
+              "refreshToken": core.androidRefreshToken,
+              "clientId": core.androidVerifierOptions.clientId,
+              "clientSecret": core.androidVerifierOptions.clientSecret,
+              "packageName": purchase.billingClientPurchase.packageName,
+              "productId": purchase.productID,
+              "purchaseToken": purchase.billingClientPurchase.purchaseToken,
+              "path": core.deliverOptions.path?.applyTags(),
+              "value": product.value
+            }
+          );
+          Log.msg(task.data);
+          if( isEmpty( task.data ) ) return false;
+          Map map = Json.decodeAsMap(task.data);
+          if( map == null || !map.containsKey("purchaseState") || map["purchaseState"] != 0 ) return false;
+    } else if (Config.isIOS) {
+      if (core.iosVerifierOptions == null ||
+          isEmpty(core.iosVerifierOptions.sharedSecret)) return false;
+          FunctionsTask task = await FunctionsTask.call(
+            core.iosVerifierOptions.verificationServer,
+            postData: {
+              "receiptData": purchase.verificationData.serverVerificationData,
+              "password": core.iosVerifierOptions.sharedSecret,
+              "productId": purchase.productID,
+              "path": core.deliverOptions.path?.applyTags(),
+              "value": product.value
+            }
+          );
+          Log.msg(task.data);
+          if( isEmpty( task.data ) ) return false;
+    }
+    return true;
+  }
+}
--- lib/masamune_purchase_firebase.dart
+++ lib/masamune_purchase_firebase.dart
@@ -8,7 +8,16 @@
 /// [YouTube]: https://www.youtube.com/c/mathrunetchannel
 library masamune.purchase.firebase;
 
+import 'package:http/http.dart';
+import 'package:masamune_firebase_mobile/masamune_firebase_mobile.dart';
+import 'package:simple_rsa/simple_rsa.dart';
+import 'package:in_app_purchase/in_app_purchase.dart';
+import 'package:masamune_flutter/masamune_flutter.dart';
+import 'package:masamune_purchase/masamune_purchase.dart';
+
 export 'package:masamune_flutter/masamune_flutter.dart';
 export 'package:masamune_purchase/masamune_purchase.dart';
 export 'package:masamune_firebase/masamune_firebase.dart';
 export 'package:masamune_firebase_mobile/masamune_firebase_mobile.dart';
+
+part 'firebaseverifier.dart';
